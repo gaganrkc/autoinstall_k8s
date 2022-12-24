0c9d4be0582ecc924754fff77755304b6a309d84
GAGAN CHAURASIA <gaganrkc@gmail.com>
2022-12-24 23:21:14 +0530

    Update common_script.sh

--- common_script.sh
+++ common_script.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
-systemctl disable firewalld
+sudo systemctl disable firewalld
 
-setenforce 0
-sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
 
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-swapoff -a
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
 
-modprobe overlay
-modprobe br_netfilter
-tee /etc/sysctl.d/kubernetes.conf<<EOF
+sudo modprobe overlay
+msudo odprobe br_netfilter
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
-sysctl --system
+sudo sysctl --system
 
 
 
-tee /etc/yum.repos.d/kubernetes.repo<<EOF
+sudo tee /etc/yum.repos.d/kubernetes.repo<<EOF
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -31,24 +31,24 @@ EOF
 
 
 
-yum clean all 
-yum -y makecache
-yum -y install epel-release git curl wget kubelet kubeadm kubectl --disableexcludes=kubernetes
-systemctl start kubelet
-systemctl enable kubelet
+sudo yum clean all 
+sudo yum -y makecache
+sudo yum -y install epel-release git curl wget kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl start kubelet
+sudo systemctl enable kubelet
 
 
 
 # Add CRI-O repo
 OS=CentOS_7
 VERSION=1.22
-curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
-curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/devel:kubic:libcontainers:stable.repo
+sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/devel:kubic:libcontainers:stable:cri-o:$VERSION.repo
 #Install CRI-O
-yum remove docker-ce docker-ce-cli containerd.io
-yum install cri-o -y
+sudo yum remove docker-ce docker-ce-cli containerd.io
+sudo yum install cri-o -y
 # Start and enable Service
-systemctl daemon-reload
-systemctl start crio
-systemctl enable crio
+sudo systemctl daemon-reload
+sudo systemctl start crio
+sudo systemctl enable crio
 
